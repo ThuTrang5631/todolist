b583a5afed75213892e6ddd4f162e72b4da60f99
Trang Lê Thị Thu <19522382@ms.uit.edu.vn>
2022-09-17 21:55:25 +0700

    doing css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [work, setWork] = useState('')
+  const [works, setWorks] = useState(()=>{
+    const storageWorks = JSON.parse(localStorage.getItem('works')) ?? []
+    return storageWorks 
+  })
+
+  const handleSubmit = () => {
+    setWorks(prev => {
+      const newWorks = [...prev, work]
+      
+      //save to localStorage
+      localStorage.setItem('works', JSON.stringify(newWorks))
+      return newWorks
+    })
+    setWork('')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>TO DO LIST</h1>
+      <div className='block'>
+      <div className='input-btn'>
+        <input placeholder='Enter todo here' value={work} onChange = {e => setWork(e.target.value)}></input>
+        <button onClick = {handleSubmit}>Add</button>
+      </div>
+      <ul>
+        {works.map((work, index) => (
+          <li key={index}>{work}</li>
+        ))}
+      </ul>
+      </div>
     </div>
   );
 }
